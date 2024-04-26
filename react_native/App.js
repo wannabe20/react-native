9a1e9e34fb787f2a5f2cfd1976b28930d198f5f7
wannabe20 <prchlik1819@gmail.com>
2024-04-26 10:32:45 +0200

    ach

--- react_native/App.js
+++ react_native/App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView} from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   return (
+    <SafeAreaView>
+    <ScrollView>
     <View style={styles.container}>
-      <Text>PIVSOON!</Text>
+      <Text>
+
+Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam in lorem sit amet leo accumsan lacinia. Nulla quis diam. Maecenas aliquet accumsan leo. Sed convallis magna eu sem. Fusce consectetuer risus a nunc. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vitae arcu tempor neque lacinia pretium. Maecenas lorem. Nullam eget nisl. Sed ac dolor sit amet purus malesuada congue. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Etiam bibendum elit eget erat. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Integer vulputate sem a nibh rutrum consequat. Nullam sapien sem, ornare ac, nonummy non, lobortis a enim.
+
+Duis pulvinar. Etiam posuere lacus quis dolor. In laoreet, magna id viverra tincidunt, sem odio bibendum justo, vel imperdiet sapien wisi sed libero. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Pellentesque pretium lectus id turpis. Proin mattis lacinia justo. Curabitur vitae diam non enim vestibulum interdum. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Aliquam in lorem sit amet leo accumsan lacinia. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Nulla est. Etiam sapien elit, consequat eget, tristique non, venenatis quis, ante. Pellentesque sapien. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. In convallis. Integer imperdiet lectus quis justo. Etiam egestas wisi a erat. Aliquam ante. Curabitur vitae diam non enim vestibulum interdum. Integer pellentesque quam vel velit. In dapibus augue non sapien. Integer vulputate sem a nibh rutrum consequat. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Mauris dolor felis, sagittis at, luctus sed, aliquam non, tellus.
+
+Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam commodo dui eget wisi. Nunc auctor. Praesent dapibus. Vivamus ac leo pretium faucibus. Proin pede metus, vulputate nec, fermentum fringilla, vehicula vitae, justo. Praesent dapibus. Aliquam ante. Phasellus rhoncus. Nullam rhoncus aliquam metus.
+
+Duis ante orci, molestie vitae vehicula venenatis, tincidunt ac pede. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam sit amet magna in magna gravida vehicula. Fusce nibh. Nullam sit amet magna in magna gravida vehicula. Nam sed tellus id magna elementum tincidunt. Etiam bibendum elit eget erat. Integer vulputate sem a nibh rutrum consequat. Fusce tellus. Maecenas libero. Nulla non arcu lacinia neque faucibus fringilla. Aliquam in lorem sit amet leo accumsan lacinia. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus.
+</Text>
       <StatusBar style="auto" />
     </View>
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +33,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  
+  
 });
